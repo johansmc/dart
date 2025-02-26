d66948836c6d43b9557dc47d3f872e60e7a6c38c
johansmc <163949679+johansmc@users.noreply.github.com>
2025-02-26 06:54:29 -0500

    actualización estación

--- Taller condicionales/estacion.dart
+++ Taller condicionales/estacion.dart
@@ -4,64 +4,63 @@ void main() {
   stdout.write("Ingrese el hemisferio:");
   String hemisferio = stdin.readLineSync()!.toLowerCase();
   stdout.write("Ingrese el mes:");
-  String mes = stdin.readLineSync()!.toLowerCase();
+  String mes = stdin.readLineSync()!;
   stdout.write("Ingrese el dia:");
-  int dia = int.parse(stdin.readLineSync()!);
-  List<String> Mamj = ["marzo", "abril", "mayo", "junio"];
-  List<String> Jjas = ["junio", "julio", "agosto", "septiembre"];
-  List<String> Sond = ["septiembre", "octubre", "noviembre", "diciembre"];
-  List<String> Defm = ["diciembre", "enero", "febrero", "diciembre"];
+  int dia = int.parse(stdin.readLineSync()!.toLowerCase());
+  
+  const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
 
   if (hemisferio == "norte") {
-    if (mes == Mamj[0] && dia >= 21 ||
-        mes == Mamj[1] ||
-        mes == Mamj[2] ||
-        mes == Mamj[3] && dia <= 21) {
+    if (mes == meses[2] && dia >= 21 ||
+        mes == meses[3] ||
+        mes == meses[4] ||
+        mes == meses[5]
+    && dia <= 21) {
       print("La estacion es primacera");
     }
-    if (mes == Jjas[0] && dia >= 21 ||
-        mes == Jjas[1] ||
-        mes == Jjas[2] ||
-        mes == Jjas[3] && dia <= 21) {
+    if (mes == meses[6] && dia >= 21 ||
+        mes == meses[7] ||
+        mes == meses[8] ||
+        mes == meses[9] && dia <= 21) {
       print("La estacion es verano");
     }
-    if (mes == Sond[0] && dia >= 21 ||
-        mes == Sond[1] ||
-        mes == Sond[2] ||
-        mes == Sond[3] && dia <= 21) {
+    if (mes == meses[10] && dia >= 21 ||
+        mes == meses[12] ||
+        mes == meses[0] ||
+        mes == meses[1] && dia <= 21) {
       print("La estacion es otonio");
     }
-    if (mes == Defm[0] && dia >= 21 ||
-        mes == Defm[1] ||
-        mes == Defm[2] ||
-        mes == Defm[3] && dia <= 21) {
+    if (mes == meses[2] && dia <= 20 ||
+        mes == meses[3] ||
+        mes == meses[4] ||
+        mes == meses[5] && dia >= 21) {
       print("La estacion es invierno");
     }
   }
 
   if (hemisferio == "sur") {
-    if (mes == Mamj[0] && dia >= 21 ||
-        mes == Mamj[1] ||
-        mes == Mamj[2] ||
-        mes == Mamj[3] && dia <= 21) {
+    if (mes == meses[2] && dia >= 21 ||
+        mes == meses[3] ||
+        mes == meses[4] ||
+        mes == meses[5] && dia <= 21) {
       print("La estacion es otoño");
     }
-    if (mes == Jjas[0] && dia >= 21 ||
-        mes == Jjas[1] ||
-        mes == Jjas[2] ||
-        mes == Jjas[3] && dia <= 21) {
+    if (mes == meses[6] && dia >= 21 ||
+        mes == meses[7] ||
+        mes == meses[8] ||
+        mes == meses[9] && dia <= 21) {
       print("La estacion es invierno");
     }
-    if (mes == Sond[0] && dia >= 21 ||
-        mes == Sond[1] ||
-        mes == Sond[2] ||
-        mes == Sond[3] && dia <= 21) {
+    if (mes == meses[10] && dia >= 21 ||
+        mes == meses[11] ||
+        mes == meses[0] ||
+        mes == meses[1] && dia <= 21) {
       print("La estacion es primavera");
     }
-    if (mes == Defm[0] && dia >= 21 ||
-        mes == Defm[1] ||
-        mes == Defm[2] ||
-        mes == Defm[3] && dia <= 21) {
+    if (mes == meses[2] && dia <= 20 ||
+        mes == meses[3] ||
+        mes == meses[4] ||
+        mes == meses[5] && dia >= 21) {
       print("La estacion es verano");
     }
   }
